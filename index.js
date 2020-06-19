af7b1e8bbaea4fdb0062595e3c5ba05c3f49807b
RebekahKirk <rebekah.kirk@srm.com>
2020-06-19 13:39:28 +0100

    Files update

--- index.js
+++ index.js
@@ -7,49 +7,64 @@ const app = express();
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const User = require ('./models/user');
-const Movies = require ('./models/movies');
+const User = require('./models/user');
+const Movies = require('./models/movies');
 mongoose.connect(`${process.env.databaseURL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-
-app.use(bodyParser.urlencoded({extended: false})); //take everyhting as a string
+});
+app.use(bodyParser.urlencoded({ extended: false })); //take everything as a string
 app.use(bodyParser.json()); // get it as json format
 app.use(express.static(path.join(__dirname, 'public'))); //style.css
 app.engine('.hbs', hbs({
     defaultLayout: 'layout',
     extname: 'hbs'
-}))
+}));
 app.set('view engine', '.hbs');
-app.get ('/', async (req, res) => {
+app.get('/', async (req, res) => {
     res.render('index');
 });
-app.get ('/movies', async (req, res) => {
-    res.render('movies');
-});
-app.post('/', async (req,res)=> {
-    let { name, email, password} = req.body;
-    const user = new User ({
+app.post('/', async (req, res) => {
+    let { name, email, password } = req.body;
+    const user = new User({
         name,
         email,
         password
     })
     await user.save();
     res.redirect('/');
-})
+});
+app.get('/movies', async (req, res) => {
+    let data = await Movies.find({});
+    let allMovies = data.map((data) => data.toObject());
+    res.render('movies', {allMovies});
+});
 app.post('/movies', async (req, res) => {
     let { movieName, actor, releaseDate, rating, stream } = req.body;
-    const movies = new Movies({
-    movieName,
-    actor,
-    releaseDate,
-    rating,
-    stream
+    const movie = new Movies({
+        movieName,
+        actor,
+        releaseDate,
+        rating,
+        stream
     })
-    await movies.save();
-    res.redirect('movies');
-})
-app.listen(3005, () => {
+    await movie.save();
+    res.redirect('movies')
+});
+app.get('/search', async(req, res) => {
+    res.render('search')
+});
+app.post('/search', async (req, res) => {
+   let {movieName} = req.body;
+   let existingMovie = await Movies.findOne({ movieName })
+   console.log(existingMovie);
+   data = {
+       movieName: existingMovie.movieName,
+       actor: existingMovie.actor,
+       releaseDate: existingMovie.releaseDate,
+   }
+   res.render('search', {data})
+});
+app.listen( 3005, () => {
     console.log('I am listening on 3005');
-})
+});
